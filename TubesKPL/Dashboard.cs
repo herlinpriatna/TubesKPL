759bc04c87ee570e3bd2af73253a15043fd6b115
Herlin Priatna <herlinpriatna@student.telkomuniversity.ac.id>
2023-05-17 10:25:57 +0700

    Menambahkan Class Dashboard & ChatBot

---
+++ TubesKPL/Dashboard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TubesKPL
+{
+    internal class Dashboard
+    {
+        public Dashboard() { }
+
+    }
+}
