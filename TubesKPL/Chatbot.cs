69a24c5576419491bdb901e7670f5d0498e6cef7
Herlin Priatna <herlinpriatna@student.telkomuniversity.ac.id>
2023-05-18 21:58:59 +0700

    Implementasi Chatbot

--- TubesKPL/Chatbot.cs
+++ TubesKPL/Chatbot.cs
@@ -2,12 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace TubesKPL
 {
-    internal class Chatbot
+    public class ChatConfig
     {
-        public Chatbot() { }
+        public string stok_produk { get; set; }
+        public string pembayaran { get; set; }
+        public string pengiriman { get; set; }
+
+        public ChatConfig() { }
+        public ChatConfig(string stok_produk, string pembayaran, string pengiriman)
+        {
+            this.stok_produk = stok_produk;
+            this.pembayaran = pembayaran;
+            this.pengiriman = pengiriman;
+        }
+    }
+
+    public class ChatBot
+    {
+        public ChatConfig chatConfig;
+        public const string fileLocation = @"./chat_config.json";
+
+        public ChatBot() 
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+
+        public ChatConfig ReadConfigFile()
+        {
+            string configJsonData = File.ReadAllText(fileLocation);
+            chatConfig = JsonSerializer.Deserialize<ChatConfig>(configJsonData);
+            return chatConfig;
+        }
+
+        public void SetDefault()
+        {
+           chatConfig = new ChatConfig("stok_produk", "pembayaran", "pengiriman");
+
+        }
+
+        public void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(chatConfig, options);
+            File.WriteAllText(fileLocation, jsonString);
+        }
     }
 }
--- TubesKPL/Program.cs
+++ TubesKPL/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TubesKPL
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ChatBot bot = new ChatBot();
+            ChatConfig config = new ChatConfig();
+
+            Console.WriteLine("------ChatBot Otomatis------");
+            Console.WriteLine("Apa yang ingin Anda tanyakan?");
+            Console.WriteLine("1. Stok Produk");
+            Console.WriteLine("2. Metode Pembayaran");
+            Console.WriteLine("3. Metode Pengiriman");
+            Console.WriteLine("Pilih salah satu : ");
+
+            string ketik = Console.ReadLine();
+            if(ketik== "1")
+            {
+                bot.ReadConfigFile();
+            } else if (ketik == "2")
+            {
+                Console.WriteLine(config.pembayaran);
+            } else if(ketik == "3")
+            {
+                Console.WriteLine(config.pengiriman);
+            } else
+            {
+                Console.WriteLine("Pilihan tidak tersedia");
+            }
+        }
+    }
+}
